46267f189721b8926a477d15a9486dfd31b7fb2a
Adam <evilspamalt@gmail.com>
2022-04-12 17:38:52 +1000

    Document build.sh slightly better before zapping

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 # script that automates .lv2 boilerplate (makes the .ttl file), generates a port list,
 # compiles the shared library, and copies the library and .ttl files to the user's home .lv2 folder
+# so it can be loaded by their DAW of choice (i.e. either Ardour or Carla, or maybe the latest LMMS beta)
 
 # run port generator, copy generated .ttl
 cd ./crates/port_generator
